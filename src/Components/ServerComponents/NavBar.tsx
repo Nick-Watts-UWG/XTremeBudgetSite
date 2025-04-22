4670391a85079db0c502b00bc497d6dd112abf50
Nick Watts <nw00110@my.westga.edu>
2025-04-21 21:25:12 -0400

    Website Basically Done
    1.) The website is basically done. I just need to rig the button to
      download the budget software once I am done.

---
+++ src/Components/ServerComponents/NavBar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+export default function NavBar() {
+
+    return (
+        <>
+        <nav className="w-full bg-gray-900 text-white px-8 py-4 flex justify-between items-center shadow-md">
+            <Link href={"/"} className="text-xl font-bold font-mono">X-TREME Budget Software</Link>
+            <div className="flex gap-6 font-mono text-sm">
+                <Link href="/Download">Download</Link>
+            </div>
+        </nav>
+        </>
+    )
+}
---
+++ src/app/Download/page.tsx
@@ -0,0 +1,17 @@
+import NavBar from "@/Components/ServerComponents/NavBar";
+
+export default function Download() {
+    return (
+        <>
+        <NavBar/>
+        <main className=" min-w-fit bg-slate-700 flex flex-col items-center w-full h-lvh pb-32 gap-36">
+            <div className="flex flex-col w-full items-center text-center mt-20">
+                <h1 className=" text-fuchsia-500 text-6xl font-mono font-bold">Download X-Treme Budget Software</h1>
+            </div>
+
+            <button className=" bg-green-500 rounded-xl font-mono p-2 text-3xl ">Download</button>
+            <p className=" w-1/2 text-emerald-700">THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
+        </main>
+        </>
+    )
+}
